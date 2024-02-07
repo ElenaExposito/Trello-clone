30acf2221228e95776d6406d0958a50288e7dd06
Elena Expósito <elexla97@gmail.com>
2024-02-07 14:08:56 +0100

    Add imports and className to TrelloList.jsx

--- src/components/TrelloList.jsx
+++ src/components/TrelloList.jsx
@@ -1,11 +1,14 @@
+import AddCardOrList from "./AddCardOrList"
 import ListTitle from "./ListTitle"
 import TrelloCard from "./TrelloCard"
+import './TrelloList.css';
 
 const TrelloList = () => {
   return (
-    <div>
+    <div className="card">
       <ListTitle/>
       <TrelloCard/>
+      <AddCardOrList/>
     </div>
   )
 }
