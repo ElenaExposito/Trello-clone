2fd090eb4dbc8bdc44af3c0b5d1e5c7e8dc184f2
Elena Expósito <elexla97@gmail.com>
2024-02-08 15:56:08 +0100

    Create logic of collapsed option of AddCardOrList.jsx

--- src/components/AddCardOrList.jsx
+++ src/components/AddCardOrList.jsx
@@ -1,16 +1,31 @@
+import { useState } from 'react';
 import '../App.css'
+import AddCardOrListText from "./AddCardOrListText";
 
 const AddCardOrList = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='add-title'>
-      <span className='add-icon'>
-        <svg width="15" height="15" viewBox="0 0 24 24" style={{ fill: 'var(--text-shadow)' }}>
-          <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
-        </svg>
-      </span>
-        <h2 className='add-title-text'>Add card</h2>
+    <div className={isOpen ? 'add-title-closed' : 'add-title-open'} onClick={handleClick}>
+      {isOpen ? null : (
+        <>
+          <span className='add-icon'>
+            <svg width="15" height="15" viewBox="0 0 24 24" style={{ fill: 'var(--text-shadow)' }}>
+              <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+            </svg>
+          </span>
+          <h2 className='add-title-text'>
+            {isOpen ? 'Add card' : 'Add list'}
+          </h2>
+        </>
+      )}
+      {isOpen && <AddCardOrListText />}
     </div>
-  )
+  );
 }
 
 export default AddCardOrList
