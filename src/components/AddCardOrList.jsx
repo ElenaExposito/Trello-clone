f8a676630f01ce175079ac2967b80621ee9287ce
Elena Expósito <elexla97@gmail.com>
2024-02-08 12:41:33 +0100

    Add more structure and styles of TrelloCard.jsx and AddCardOrList.jsx components

--- src/components/AddCardOrList.jsx
+++ src/components/AddCardOrList.jsx
@@ -2,8 +2,13 @@ import '../App.css'
 
 const AddCardOrList = () => {
   return (
-    <div>
-      <h2>Add Card</h2>
+    <div className='add-title'>
+      <span className='add-icon'>
+        <svg width="15" height="15" viewBox="0 0 24 24" style={{ fill: 'var(--text-shadow)' }}>
+          <path d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z"/>
+        </svg>
+      </span>
+        <h2 className='add-title-text'>Add card</h2>
     </div>
   )
 }
--- src/components/TrelloCard.jsx
+++ src/components/TrelloCard.jsx
@@ -1,8 +1,13 @@
 
 const TrelloCard = () => {
   return (
-    <div>
-      <h2>Card</h2>
+    <div className="card-title">
+      <h2 className="card-title-text">Card</h2>
+      <button className="edit-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'var(--text-color)' }}>
+          <path d="M4.481 15.659C3.147 19.575 3 19.891 3 20.246c0 .528.46.749.749.749.352 0 .668-.137 4.574-1.492zM5.541 14.598l3.846 3.846 11.321-11.311c.195-.195.293-.45.293-.707 0-.255-.098-.51-.293-.706-.692-.691-1.742-1.74-2.435-2.432-.195-.195-.451-.293-.707-.293-.254 0-.51.098-.706.293z"/>
+        </svg>
+      </button>
     </div>
   )
 }
