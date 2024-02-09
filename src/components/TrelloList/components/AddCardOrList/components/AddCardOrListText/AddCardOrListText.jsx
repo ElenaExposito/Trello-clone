b4dc97909ac3fd19b1e9a4d4b975b8ec40cbacf0
Elena Expósito <elexla97@gmail.com>
2024-02-09 14:13:02 +0100

    Add more styles for AddCardOrListText component and change some cursors to pointer

--- src/components/TrelloList/components/AddCardOrList/components/AddCardOrListText/AddCardOrListText.jsx
+++ src/components/TrelloList/components/AddCardOrList/components/AddCardOrListText/AddCardOrListText.jsx
@@ -6,8 +6,8 @@ const AddCardOrListText = () => {
         <label>
             <textarea className="add-card-or-list-title-textarea" type="text" placeholder="Enter a title for this card..."></textarea>
         </label>
-        <div>
-          <button>Añadir Tarjeta</button>
+        <div className="add-card-or-list-buttons">
+          <button className="add-card-or-list-button">Añadir Tarjeta</button>
           <button className="list-icon">
             <svg width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'var(--text-color)' }}>
                 <path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/>
