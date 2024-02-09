c90e8eb9ba77e1fee1b479990622000d783ebe27
Elena Expósito <elexla97@gmail.com>
2024-02-09 13:34:13 +0100

    Add some styles for AddCardOrListText component

--- src/components/TrelloList/components/AddCardOrList/components/AddCardOrListText/AddCardOrListText.jsx
+++ src/components/TrelloList/components/AddCardOrList/components/AddCardOrListText/AddCardOrListText.jsx
@@ -1,16 +1,19 @@
+import "./AddCardOrListText.css"
 
 const AddCardOrListText = () => {
   return (
-    <div className="">
+    <div className="add-card-or-list-title">
         <label>
-            <input type="text" placeholder="Enter a title for this card..."></input>
+            <textarea className="add-card-or-list-title-textarea" type="text" placeholder="Enter a title for this card..."></textarea>
         </label>
-        <button>Añadir Tarjeta</button>
-        <button className="list-icon">
+        <div>
+          <button>Añadir Tarjeta</button>
+          <button className="list-icon">
             <svg width="20" height="20" viewBox="0 0 24 24" style={{ fill: 'var(--text-color)' }}>
                 <path d="M23 20.168l-8.185-8.187 8.185-8.174-2.832-2.807-8.182 8.179-8.176-8.179-2.81 2.81 8.186 8.196-8.186 8.184 2.81 2.81 8.203-8.192 8.18 8.192z"/>
             </svg>
-        </button>
+          </button>
+        </div>
     </div>
   )
 }
