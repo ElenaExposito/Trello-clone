678c3904998216431a9372f8864f7c87fbcce587
Elena Expósito <elexla97@gmail.com>
2024-02-19 14:59:03 +0100

    Add scroll styles'

--- src/components/TrelloList/TrelloList.jsx
+++ src/components/TrelloList/TrelloList.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import AddCardOrList from './components/AddCardOrList/AddCardOrList'
 import ListTitle from './components/ListTitle/ListTitle'
@@ -6,35 +7,63 @@ import './TrelloList.css'
 import { Draggable, Droppable } from 'react-beautiful-dnd'
 
 const TrelloList = ({ list, index: listIndex }) => {
+  const trellocardCardsRef = useRef(null)
+
+  useEffect(() => {
+    const checkScroll = () => {
+      const hasScroll =
+        trellocardCardsRef.current.scrollHeight >
+        trellocardCardsRef.current.clientHeight
+      if (hasScroll) {
+        trellocardCardsRef.current.classList.add('has-scroll')
+      } else {
+        trellocardCardsRef.current.classList.remove('has-scroll')
+      }
+    }
+
+    // Verifica el desplazamiento al cargar la página o al renderizar el componente
+    checkScroll()
+
+    // Agrega un listener para manejar el cambio en el desplazamiento
+    trellocardCardsRef.current.addEventListener('scroll', checkScroll)
+
+    // Limpia el listener al desmontar el componente
+    return () => {
+      trellocardCardsRef.current.removeEventListener('scroll', checkScroll)
+    }
+  }, [])
+
   return (
-    <Draggable draggableId={list.id} index={listIndex}>
-      {(provided) => (
-        <div
-          className="card"
-          ref={provided.innerRef}
-          {...provided.draggableProps}
-          {...provided.dragHandleProps}
-        >
-          <ListTitle title={list.title} listId={list.id} />
-          <Droppable droppableId={list.id} type="card">
-            {(dropProvided) => (
-              <div
-                className="cards"
-                ref={dropProvided.innerRef}
-                {...dropProvided.droppableProps}
-              >
-                <div className="trellocard-cards">
-                  {list.cards.map((card, index) => (
-                    <TrelloCard card={card} key={card.id} index={index} />
-                  ))}
+    <div className="card-wrapper">
+      <Draggable draggableId={list.id} index={listIndex}>
+        {(provided) => (
+          <div
+            className="card"
+            ref={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            <ListTitle title={list.title} listId={list.id} />
+            <Droppable droppableId={list.id} type="card">
+              {(dropProvided) => (
+                <div
+                  className="cards"
+                  ref={dropProvided.innerRef}
+                  {...dropProvided.droppableProps}
+                >
+                  <div className="trellocard-cards" ref={trellocardCardsRef}>
+                    {list.cards.map((card, index) => (
+                      <TrelloCard card={card} key={card.id} index={index} />
+                    ))}
+                  </div>
+                  <AddCardOrList type="card" listId={list.id} />
                 </div>
-                <AddCardOrList type="card" listId={list.id} />
-              </div>
-            )}
-          </Droppable>
-        </div>
-      )}
-    </Draggable>
+              )}
+            </Droppable>
+          </div>
+        )}
+      </Draggable>
+    </div>
   )
 }
 
--- src/components/TrelloList/mockdata.js
+++ src/components/TrelloList/mockdata.js
@@ -1,28 +1,94 @@
 const mockData = {
-    lists: {
-        "01list": {
-            id: "01list",
-            title: "To do",
-            cards: [{
-                id: "01card",
-                title: "Comprar lechuga"
-            }, {
-                id: "02card",
-                title: "Comprar tomates"
-                },
-                {
-                    id: "03card",
-                    title: "Comprar pan"
-                },
-            ]
+  lists: {
+    '01list': {
+      id: '01list',
+      title: 'To do',
+      cards: [
+        {
+          id: '01card',
+          title: 'Comprar lechuga',
         },
-        "02list" : {
-            id: "02list",
-            title: "In progress",
-            cards:[]
-        }
+        {
+          id: '02card',
+          title: 'Comprar tomates',
+        },
+        {
+          id: '04card',
+          title: 'Comprar pan',
+        },
+        {
+          id: '05card',
+          title: 'Comprar queso',
+        },
+        {
+          id: '06card',
+          title: 'Comprar jamón york',
+        },
+        {
+          id: '07card',
+          title: 'Comprar jamón serrano',
+        },
+        {
+          id: '08card',
+          title: 'Comprar pimientos',
+        },
+        {
+          id: '09card',
+          title: 'Comprar aceite',
+        },
+        {
+          id: '10card',
+          title: 'Comprar sal',
+        },
+        {
+          id: '11card',
+          title: 'Comprar azúcar',
+        },
+        {
+          id: '12card',
+          title: 'Comprar gelatina',
+        },
+        {
+          id: '13card',
+          title: 'Comprar fruta en almíbar',
+        },
+        {
+          id: '14card',
+          title: 'Comprar spray anti insectos',
+        },
+        {
+          id: '15card',
+          title: 'Comprar dulces',
+        },
+        {
+          id: '16card',
+          title: 'Comprar botella de agua',
+        },
+        {
+          id: '17card',
+          title: 'Comprar empanada de atún',
+        },
+        {
+          id: '18card',
+          title: 'Comprar empanada boloñesa',
+        },
+        {
+          id: '19card',
+          title: 'Comprar empanada de verduras',
+        },
+        {
+          id: '20card',
+          title: 'Comprar tortilla de patatas',
+        },
+      ],
     },
-    listIds: ["01list", "02list"]
+    '02list': {
+      id: '02list',
+      title: 'In progress',
+      cards: [],
+    },
+  },
+  listIds: ['01list', '02list'],
 }
 
 export default mockData
